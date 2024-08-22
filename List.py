658d4306a39b360b9bd814b836762987459a94af
Dalmas <akumuodhiambo@gmail.com>
2024-08-22 13:52:07 +0300

    Answeres

---
+++ List.py
@@ -0,0 +1,27 @@
+my_list = []
+
+#Append 
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#Insert
+my_list.insert(1, 15)
+
+#Extend
+
+my_list.extend([50, 60, 70])
+
+#Remove 
+my_list.pop(7)
+
+#Sort
+my_list.sort()
+
+#PrintIndexOf30
+index_of_30 = my_list.index(30)
+print(index_of_30)
+
+
+print (my_list)
